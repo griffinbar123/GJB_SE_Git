139011f342f5ee504e542dc7f15ef1050bfb8ca0
griffinbar123 <gjb20b@acu.edu>
2023-09-06 12:19:09 -0500

    edited file a

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
 int main(int argc, char **argv) {
-    std::cout << "Hello, world!" << std::endl;
+    std::cout << "Hello, world, edit from b!" << std::endl;
     return 0;
 }
